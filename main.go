7d9ad801b4c3fc8baca32f11a48f3466f4c7b2d6
Antoine Robert <antoine.al.robert@gmail.com>
2023-08-22 15:58:42 +0200

    Lesson #12 - Maps

--- main.go
+++ main.go
@@ -2,12 +2,28 @@ package main
 
 import "fmt"
 
-var score = 99.5
-
 func main() {
-	greet("Antoine")
-	for _, v := range points {
-		fmt.Println(v)
+	menu := map[string]float64{
+		"soup":  4.99,
+		"pie":   7.99,
+		"salad": 6.99,
 	}
-	showScore()
+
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+	for k, v := range menu {
+		fmt.Printf("menu[\"%v\"] = %v\n", k, v)
+	}
+
+	phonebook := map[int]string{
+		234567: "mario",
+		123456: "luigi",
+		345678: "peach",
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[123456])
+
+	phonebook[123456] = "bowser"
+	fmt.Println(phonebook)
 }
