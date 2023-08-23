0f4da132622eef8fe2449d20b2f995e79c0e5c3c
Antoine Robert <antoine.al.robert@gmail.com>
2023-08-23 09:27:09 +0200

    Lesson #15 - Struct & Cusom Types

--- main.go
+++ main.go
@@ -2,19 +2,8 @@ package main
 
 import "fmt"
 
-func updateName(x *string) {
-	*x = "wedge"
-}
-
 func main() {
-	// Pointers
-	name := "alph0n5e"
-	m := &name
+	myBill := newBill("My Bill")
 
-	fmt.Println("Memory address of 'name': ", m)
-	fmt.Println("Value at memory address: ", *m)
-
-	fmt.Println(name)
-	updateName(m)
-	fmt.Println(name)
+	fmt.Println(myBill)
 }
