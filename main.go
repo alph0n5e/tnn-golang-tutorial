92f93cbc03107cca7ca1e54a408355c2534c06c4
Antoine Robert <antoine.al.robert@gmail.com>
2023-08-23 16:23:55 +0200

    Lesson #18 - User Input

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+	return strings.TrimSpace(input), err
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+
+	name, _ := getInput("Create a new bill name: ", reader)
+
+	b := newBill(name)
+	fmt.Println("Created the bill - ", b.name)
+
+	return b
+}
+
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
+	fmt.Println(opt)
+}
 
 func main() {
-	myBill := newBill("My Bill")
-
-	myBill.addItem("coffee", 1.99)
-	myBill.addItem("tea", 2.99)
-	myBill.addItem("pie", 3.99)
-	myBill.addItem("cake", 2.50)
-
-	myBill.updateTip(1.2)
-
-	fmt.Println(myBill.format())
+	myBill := createBill()
+	promptOptions(myBill)
 }
